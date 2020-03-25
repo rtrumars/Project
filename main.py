fc0546f04b21cc57cd0e4e5cad14aca19f483537
rtrumars <60189993+rtrumars@users.noreply.github.com>
2020-03-25 23:51:32 +0200

    TEST

--- main.py
+++ main.py
@@ -3,8 +3,30 @@ from flask import Flask, render_template, request
 app = Flask('app')
 
 @app.route('/')
-def main():
-  return render_template('main.html')
+def home():
+  return render_template('home.html')
+
+@app.route('/lapa')
+def lapa():
+  return render_template('lapa.html')
+
+@app.route('/about')
+def contact():
+  return render_template('contact.html')
+
+@app.route('/eng')
+def homeeng():
+  return render_template('homeeng.html')
+
+@app.route('/lapa/eng')
+def lapaeng():
+  return render_template('lapaeng.html')
+
+@app.route('/about/eng')
+def contacteng():
+  return render_template('contacteng.html')
+
+  
 
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
