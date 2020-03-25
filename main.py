da3c46fa58b318c236d6572010d305a0e1f84f60
rtrumars <60189993+rtrumars@users.noreply.github.com>
2020-03-26 00:36:47 +0200

    BIG UPDATE

--- main.py
+++ main.py
@@ -5,27 +5,6 @@ app = Flask('app')
 @app.route('/')
 def home():
   return render_template('home.html')
-
-@app.route('/lapa')
-def lapa():
-  return render_template('lapa.html')
-
-@app.route('/about')
-def contact():
-  return render_template('contact.html')
-
-@app.route('/eng')
-def homeeng():
-  return render_template('homeeng.html')
-
-@app.route('/lapa/eng')
-def lapaeng():
-  return render_template('lapaeng.html')
-
-@app.route('/about/eng')
-def contacteng():
-  return render_template('contacteng.html')
-
   
 
 if __name__ == "__main__":
